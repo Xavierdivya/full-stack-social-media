f113448259b121a22ea6351f9074fb1265168e1e
Divya <146223550+Xavierdivya@users.noreply.github.com>
2025-02-14 21:38:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Welcome to Fullstack Social Media</h1>
+            <p>This is a simple frontend setup.</p>
+        </div>
+    );
+}
+
+export default App;
